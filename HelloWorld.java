b62233baa5187ea2cc503b601bfc275ab4f1706c
Krishna Sri Niharika Siruvuri <niharikasiruvuri@gmail.com>
2020-06-15 15:39:15 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package first.sample.javaprogram;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello world all");
+
+	}
+
+}
---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package first.sample.javaprogram;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table=new MultiplicationTable();
+		table.print();
+
+	}
+
+}
