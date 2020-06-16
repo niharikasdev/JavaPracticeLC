3aed763adb62ae76278fbf6420a538090477f882
Krishna Sri Niharika Siruvuri <niharikasiruvuri@gmail.com>
2020-06-15 19:14:20 -0500

    Add files via upload

---
+++ palindromeFind.java
@@ -0,0 +1,32 @@
+package practice;
+
+public class palindromeFind
+{
+	    static boolean isPalindrome(String str) 
+	    { 
+
+	        int i = 0, j = str.length() - 1; 
+	  
+	        while (i < j) 
+	        { 
+
+	            if (str.charAt(i) != str.charAt(j)) 
+	                return false; 
+	  
+	            i++; 
+	            j--; 
+	        } 
+	  
+	        return true; 
+	    } 
+	  
+	    public static void main(String[] args) 
+	    { 
+	        String str = "nitin"; 
+	  
+	        if (isPalindrome(str)) 
+	            System.out.print("Yes"); 
+	        else
+	            System.out.print("No"); 
+	    } 
+}
